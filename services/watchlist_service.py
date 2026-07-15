56312cd484fa71970032b67d9ca98556635e016f
samanth1111-1111 <leazamasamantha1111@gmail.com>
2026-07-14 19:00:35 -0500

    refactor: use db.session.get instead of deprecated Query.get
    Query.get is deprecated in SQLAlchemy 2.0; use Session.get in the collection and watchlist services. No behavior change.

--- services/watchlist_service.py
+++ services/watchlist_service.py
@@ -29,7 +29,7 @@ def add_to_watchlist(user_id, film_id):
         FilmNotFoundError: If film_id does not exist.
         AlreadyInWatchlistError: If the film is already on the user's watchlist.
     """
-    film = Film.query.get(film_id)
+    film = db.session.get(Film, film_id)
     if film is None:
         raise FilmNotFoundError(f"No film found with id '{film_id}'")
 
